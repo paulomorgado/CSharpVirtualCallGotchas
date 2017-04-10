aa27308de2342d636ba2ca0d3879d7ca392e47a8
Paulo Morgado <paulo@paulomorgado.net>
2017-04-11 00:03:39 +0100

    fixed test2.cmd

--- test2.cmd
+++ test2.cmd
@@ -7,12 +7,12 @@ csc Program.cs /target:exe /reference:A.dll /reference:B.dll /reference:C.dll /o
 echo "A->B->C"
 Program.exe
 
-csc B2.cs /target:library /reference:A.dll /reference:B.dll /out:C.dll /nologo
+csc B.cs /target:library /reference:A.dll /out:B.dll /nologo
 
 echo "A->C"
 Program.exe
 
-csc C2.cs /target:library /reference:A.dll /out:B.dll /nologo
+csc C.cs /target:library /reference:A.dll /reference:B.dll /out:C.dll /nologo
 
 echo "A"
 Program.exe
